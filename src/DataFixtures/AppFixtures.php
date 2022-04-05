03de98ef8ff2919f921c443190b477d8e2fa09d5
JulienLaurent77 <julienlaurent77@outlook.com>
2022-04-05 14:11:28 +0200

    feat(DataFixtures): add animal fixtures with personnalised pictures, relations ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Animal;
 use App\Entity\Association;
 use App\Entity\Department;
 use App\Entity\Species;
@@ -13,7 +14,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
+        $faker = Factory::create('fr_FR');
         $faker->seed(806);
         
         // $product = new Product();
@@ -21,7 +22,7 @@ class AppFixtures extends Fixture
 
         // prépartion des données
 
-        // On veut créer une liste de 4 espèces et les stocker dans un tableau
+        // ! On veut créer une liste de 4 espèces et les stocker dans un tableau
         $species = [
             'Chat',
             'Chien',
@@ -42,8 +43,9 @@ class AppFixtures extends Fixture
             // On l'enregistre dans le manager
            $manager->persist($species);
         }
+        
 
-        // On veut créer une liste des départements et les stocker dans un tableau
+        // ! On veut créer une liste des départements et les stocker dans un tableau
         $department = [
             '01 - Ain',
             '02 - Aisne',
@@ -164,11 +166,11 @@ class AppFixtures extends Fixture
         }
 
 
-        // On veut créer une liste des associations
+        // ! On veut créer une liste des associations
         $nbAssociation = 10;
 
-       // va contenir les objets associations que l'on a créé
-       $associationObjects = [];
+        // va contenir les objets associations que l'on a créé
+        $associationObjects = [];
 
        for ($associationCount = 0; $associationCount < $nbAssociation; $associationCount++) {
            $association = new Association();
@@ -183,10 +185,79 @@ class AppFixtures extends Fixture
            $association->setActive($faker->boolean());
            
            $manager->persist($association);
+           $associationObjects[] = $association;
+           
+        }
+
+       // ! On créer une liste d'url d'images personnalisées
+       $imageAninmal = [
+        'https://cdn.pixabay.com/photo/2020/02/29/18/59/rabbit-4890861_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/07/13/16/10/cute-2500929_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/03/26/00/57/easter-2174681_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2019/07/31/19/21/hare-4375952_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2019/07/07/07/18/hare-4321913_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/03/24/12/27/bunny-1276628_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2018/03/23/15/38/tiger-cat-3254032_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2019/06/28/09/58/rabbit-4303823_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/08/16/10/39/hare-2647220_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2019/09/05/07/50/border-collie-4453408_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/09/07/23/35/cat-2727159_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2021/04/17/23/58/bunny-6187026_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2021/12/15/04/48/animal-6871771_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/03/29/11/21/cat-4980341_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2014/04/05/11/10/lop-eared-314881_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2014/04/02/20/51/cat-308244_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2018/03/24/21/52/fur-3257995_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/10/01/20/54/mouse-1708347_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2014/01/11/23/40/guinea-pig-242520_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/12/29/10/23/nature-3047194_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/10/26/22/00/hamster-1772742_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/21/12/32/happy-1845090_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2014/11/23/16/12/guinea-pig-542917_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2022/02/25/02/25/mouse-7033416_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/02/25/15/50/dog-1222364_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2015/12/13/20/25/beagle-1091670_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/02/13/08/11/happy-4844817_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/05/25/17/45/perro-5219693_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/12/09/16/56/dog-5817962_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/06/18/11/57/hund-5313212_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/05/27/18/30/frenchie-5228379_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2015/09/01/01/16/tuxedo-cat-916335_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/21/12/31/beach-1845081_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/03/04/17/31/tongue-4902262_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2019/07/24/13/58/dogue-de-bordeaux-4360233_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2020/06/23/18/11/cute-cat-5333325_960_720.jpg'
+       ];
+
+
+       // ! On veut créer une liste d'animaux
+       $nbAnimal = 20;
+
+       for ($animalCount = 0; $animalCount < $nbAnimal; $animalCount++) {
+           // ajout de l'animal
+           $animal = new Animal();
+           
+           $animal->setName($faker->name());
+           $animal->setSpecies($faker->randomElement($speciesObjects));
+           $animal->setGender($faker->randomElement(['Male', 'Femelle']));
+           $animal->setAge($faker->randomFloat(1, 1, 15));
+           $animal->setPicture($faker->randomElement($imageAninmal));
+           $animal->setChildCompatibility($faker->randomElement(['True', 'False']));
+           $animal->setOtherAnimalCompatibility($faker->randomElement(['True', 'False']));
+           $animal->setGardenNeeded($faker->randomElement(['True', 'False']));
+           $animal->setStatus($faker->randomFloat(1, 0, 4));
+           $animal->setDescription($faker->realText(150));
+           
+           //    $randomDepartmentNb = mt_rand(0,100);
+           //    $department = $randomDepartmentNb;
+           $animal->setAssociation($faker->randomElement($associationObjects));
+           $animal->setDepartment($faker->randomElement($departmentObjects));
+
+           $manager->persist($animal);
        }
-        
-
-
 
         
         $manager->flush();
