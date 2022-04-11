882abc70cadec9c4fe74148172350bef5519db35
JulienLaurent77 <julienlaurent77@outlook.com>
2022-04-11 18:05:59 +0200

    feat(security): add token JWT, add secutiy bundle, amend somes parameters in User Entity (and new migrations)

---
+++ src/Controller/Api/ApiLoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Core\Security;
+
+class ApiLoginController extends AbstractController
+{
+    /**
+     * @Route("/api/login", name="app_api_login")
+     */
+    public function index (): Response
+    {
+        
+            return $this->json([
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/ApiLoginController.php',
+            ]);
+        
+        // $user = $security->getUser();
+        // if (null === $user) {
+        //     return $this->json([
+        //             'message' => 'missing credentials',
+        //              ], Response::HTTP_UNAUTHORIZED);
+        // }
+
+        // //$token = ...; // somehow create an API token for $user
+
+        // return $this->json([
+        //     'user'  => $user->getUserIdentifier(),
+        //     //'token' => $token,
+        // ]);
+    }
+  
+}
+
