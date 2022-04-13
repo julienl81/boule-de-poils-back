71cac790bf695f9dff2ee6c7b62d3b936b770cd2
VirginieT <vtouzalin33@gmail.com>
2022-04-13 15:03:19 +0200

    feat(apiUser): datas updated

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -29,18 +29,8 @@ class UserController extends AbstractController
     * @Route("", name="create", methods="POST")
     * @return Response
     */
-    public function create(ManagerRegistry $doctrine, Request $request, UserRepository $userRepository, UserPasswordHasherInterface $hasher, ValidatorInterface $validator, SerializerInterface $serializer): Response
+    public function add(ManagerRegistry $doctrine, Request $request, UserRepository $userRepository, UserPasswordHasherInterface $hasher, ValidatorInterface $validator, SerializerInterface $serializer): Response
     {
-        // if (! $this->isGranted("ROLE_ADMIN"))
-        // {
-        //     $data =
-        //     [
-        //         'error' => true,
-        //         'msg' => 'Il faut être admin pour accéder à ceci'
-        //     ];
-        //     return $this->json($data, Response::HTTP_FORBIDDEN);
-        // }
-
         // récupérer les données depuis la requete
         $userAsJson = $request->getContent();
 
@@ -50,13 +40,10 @@ class UserController extends AbstractController
         /**
          * @link https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
          */
-
         $hashedPassword = $hasher->hashPassword($user, $user->getPassword());
         $user->setPassword($hashedPassword);
         $user->setRoles(["ROLE_USER"]);
         $user->setActive("active");
-
-
 
         // enregistrer le user en BDD
         $entityManager = $doctrine->getManager();
@@ -89,69 +76,12 @@ class UserController extends AbstractController
             $data = 
             [
                 'error' => true,
-                'message' => 'User not found',
+                'message' => 'Cet identifiant est inconnu',
             ];
             return $this->json($data, Response::HTTP_NOT_FOUND, [], ['groups' => "api_user"]);
         }
 
         return $this->json($user, Response::HTTP_OK, [], ['groups' => "api_user"]);
-    }
-
-
-    /**
-     * Updates a user
-     * 
-     * @Route("/{id}", name="update", methods="PUT", requirements={"id"="\d+"})
-     * @return Response
-     */
-    public function update(ValidatorInterface $validator, int $id, ManagerRegistry $doctrine,  UserPasswordHasherInterface $hasher, Request $request, UserRepository $userRepository, SerializerInterface $serializer): Response
-    {
-        if (! $this->isGranted("ROLE_ADMIN"))
-        {
-            $data = 
-            [
-                'error' => true,
-                'msg' => 'Il faut être admin pour accéder à ce endpoint ( You SHALL not PASS )'
-            ];
-            return $this->json($data, Response::HTTP_FORBIDDEN);
-        }
-
-        // récupérer l'utilisateur dans la BDD
-        $user = $userRepository->find($id);
-
-        // gérer le cas ou l'id n'existe pas en BDD
-        if (is_null($user))
-        {
-            return JsonErrorResponse::sendError('Cet identifiant est inconnu');
-        }
-        // récupérer les données depuis la requete
-        $userAsJson = $request->getContent();
-
-        // modifier l'utilisateur
-        $serializer->deserialize($userAsJson, User::class, JsonEncoder::FORMAT, [AbstractNormalizer::OBJECT_TO_POPULATE => $user]);
-        
-        // on veut vérifier si on nous a envoyé un mot de passe
-        // pour cela on va désérialiser le json avec php et vérifier si un champ mot de passe existe
-        $userStdObj = json_decode($userAsJson);
-
-        if (isset($userStdObj->password))
-        {
-            $hashedPassword = $hasher->hashPassword($user, $userStdObj->password);
-            $user->setPassword($hashedPassword);
-        }
-
-        $errors = $validator->validate($user);
-
-        if (count($errors) > 0)
-        {
-            return JsonErrorResponse::sendError((string) $errors, Response::HTTP_BAD_REQUEST);
-        }
-        // enregistrer le user en BDD
-        $entityManager = $doctrine->getManager();
-
-        $entityManager->flush();
-
-        return new Response('', Response::HTTP_NO_CONTENT);
     }
 
 
@@ -167,6 +97,4 @@ class UserController extends AbstractController
 
         return $this->json($userList, Response::HTTP_OK, [], ['groups' => "api_user"]);
     }
-
-
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -122,8 +122,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): ?array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLES_USER
-        //$roles[] = 'ROLES_USER';
+        //guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
