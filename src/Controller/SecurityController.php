3d1cb8c6959bc316df72e76c8f68ad4ff14dae09
JulienLaurent77 <julienlaurent77@outlook.com>
2022-04-19 00:56:33 +0200

    feat(login): login ok for backoffice. design login page and animals

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -31,6 +31,7 @@ class SecurityController extends AbstractController
      */
     public function logout(): void
     {
+        // return $this->redirectToRoute('app_login');
        //  throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
