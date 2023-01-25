49dd9e2a4c2d0381448ceaac9ed5929b6d2a8cb3
JulienLaurent77 <julienlaurent77@outlook.com>
2023-01-25 10:55:20 +0100

    implement units test for learn

--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -8,11 +8,28 @@ use PHPUnit\Framework\TestCase;
 class ProductTest extends TestCase
 {
 
-    public function testcomputeTVAFoodProduct()
+    /**
+     * @dataProvider pricesForFoodProduct
+     */
+    public function testcomputeTVAFoodProduct($price, $expectedTva)
     {
-        $product = new Product('Un produit', Product::FOOD_PRODUCT, 20);
-        $this->assertSame(1.1, $product->computeTVA());
+        $product = new Product('Un produit', Product::FOOD_PRODUCT, $price);
+        $this->assertSame($expectedTva, $product->computeTVA());
     }
+    public function pricesForFoodProduct()
+    {
+        return [
+            [0, 0.0],
+            [20, 1.1],
+            [100, 5.5]
+        ];
+    }
+
+    // public function testcomputeTVAFoodProduct()
+    // {
+    //     $product = new Product('Un produit', Product::FOOD_PRODUCT, 20);
+    //     $this->assertSame(1.1, $product->computeTVA());
+    // }
 
     public function testcomputeTVAFoodProductNotFood()
     {
